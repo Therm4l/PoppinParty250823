405b66aeedb58acf76dc556463874b9ca244830b
Therm4l <tang18048@outlook.com>
2025-09-05 17:41:34 +0800

    调试显示效果

--- assets/data/repo_data/posts_db.js
+++ assets/data/repo_data/posts_db.js
@@ -1,6 +1,14 @@
 // 数据驱动：所有的Repo帖子都在这里定义
 const REPO_POSTS_DATA = [
     {
+        author: "redkold",
+        title: "现地记录: 8.23 Poppin' Party",
+        markdownFile: "assets/data/repo_data/md/redkold.md", // 指向 .md 文件
+        images: [
+        ],
+        previewImage: "assets/data/repo_data/images/redkold/1fcd9bb0150b30c70ba8c2cd9021d6f8_0.png" // 预览图
+    },
+    {
         author: "サクライ",
         title: "然後夏日終將逝去。| 非典型性live repo",
         markdownFile: "assets/data/repo_data/md/サクライ.md", // 指向 .md 文件
@@ -13,13 +21,5 @@ const REPO_POSTS_DATA = [
             "assets/data/repo_data/images/サクライ/微信图片_20250824180028_14_315.jpg"
         ],
         previewImage: "assets/data/repo_data/images/サクライ/微信图片_20250824170825_10_315.jpg" // 预览图
-    },
-    {
-        author: "redkold",
-        title: "现地记录: 8.23 Poppin' Party",
-        markdownFile: "assets/data/repo_data/md/redkold.md", // 指向 .md 文件
-        images: [
-        ],
-        previewImage: "assets/data/repo_data/images/redkold/1fcd9bb0150b30c70ba8c2cd9021d6f8_0.png" // 预览图
     }
 ];
